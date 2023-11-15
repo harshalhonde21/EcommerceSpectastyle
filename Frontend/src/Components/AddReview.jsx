641b0299c5a7dec7beaebe83afabac267db62358
harshalhonde21 <harshalhonde17@gmail.com>
2023-11-15 12:54:19 +0530

    addreview process starts here

---
+++ Frontend/src/Components/AddReview.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import "../CSS/AddReview.css"
+
+const AddReview = () => {
+    return (
+        <Fragment>
+            <div className="review-container">
+                
+            </div>
+        </Fragment>
+    );
+};
+
+export default AddReview;
