c4dee78718c1bea0e6115ae6ece7f4ac7a2c1a7b
Apurv7777 <65327303+Apurv7777@users.noreply.github.com>
2024-05-12 20:17:38 +0530

    Update user-routes.js

--- Backend/Routes/user-routes.js
+++ Backend/Routes/user-routes.js
@@ -1,11 +1,21 @@
 import express from "express";
 import { getMyUsers, login, signup } from "../Controller/user-controller.js";
 import authMiddelware from "../Middelware/authMiddelware.js";
+import multer from 'multer';
+const storage = multer.diskStorage({
+    destination: (req,file,cb)=>{
+        return cb(null,"/uploads");
+    },
+    filename:(req,file,cb)=>{
+        return cb(null,`${Date.now()}-${file.originalname}`)
+    }
+})
+const upload = multer({ storage })
 
 const router = express.Router();
 
 router.get("/", getMyUsers); // my all users in one place
-router.post("/signup", signup); //my users should signup here
+router.post("/signup",upload.single("fileInput"),signup); //my users should signup here
 router.post("/login", login); //my users should login here
 
 export default router;
