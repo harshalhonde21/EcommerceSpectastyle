58393af8047dc637e7fa3576d32dacb482c6a795
GarimaSingh0109 <130893914+GarimaSingh0109@users.noreply.github.com>
2024-05-12 22:29:39 +0530

    Update HomeSecond.jsx

--- Frontend/src/Pages/HomeSecond.jsx
+++ Frontend/src/Pages/HomeSecond.jsx
@@ -1,25 +1,75 @@
 import { Fragment } from "react";
-import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "../CSS/HomeSecond.css";
 
 const HomeSecond = () => {
     return (
         <Fragment>
-            <div className="homeSecond-container">
-                <h1 className="main-heading">Offers</h1>
-                <div className="decorative-slider">
-                    <Carousel>
-                        <div>
-                            <img src="/poster4.jpg" alt="Poster 1" />
-                        </div>
-                        <div>
-                            <img src="/poster3.jpg" alt="Poster 2" />
-                        </div>
-                    </Carousel>
-                </div>
+      <div className="homeSecond-container">
+        <h1 className="main-heading">Offers</h1>
+        <div className="decorative-slider">
+          <div id="carouselExampleIndicators" className="carousel slide">
+            <div className="carousel-indicators">
+              <button
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide-to="0"
+                className="active"
+                aria-current="true"
+                aria-label="Slide 1"
+              ></button>
+              <button
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide-to="1"
+                aria-label="Slide 2"
+              ></button>
+              <button
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide-to="2"
+                aria-label="Slide 3"
+              ></button>
             </div>
-        </Fragment>
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img src="/poster4.jpg" className="d-block w-100" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src="/poster3.jpg" className="d-block w-100" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src="/poster2.jpg" className="d-block w-100" alt="..." />
+              </div>
+            </div>
+            <button
+              className="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide="prev"
+            >
+              <span
+                className="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button
+              className="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide="next"
+            >
+              <span
+                className="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </Fragment>
     );
 };
 
