da4ca6e39bfe88402c365e44632afa13757e9bef
rahul shrivastava <rahulshrivastav052@gmail.com>
2024-06-19 21:12:18 +0530

    segment-scroll-added

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -12,43 +12,45 @@ const Home = () => {
 
   return (
     <Fragment>
-      <div className="home-container">
-        <div className="top">
-          <div className="top-left">
-            <h1 className="top-left-1">SPECTASTYLE</h1>
-            <h6 className="top-left-2-h6">Your one stop Sneaker Shop!</h6>
-            <h6 className="top-left-2">
-              Welcome to our premium e-commerce shoe haven! Discover stylish and
-              comfortable footwear, showcasing the latest trends and timeless
-              classics. From casual kicks to elegant heels, find your perfect
-              pair and step into fashion-forward confidence with us.
-            </h6>
-            <div className="btn-container">
-              <button className="btn-1" >
-                <a href="#products">Products</a>
-              </button>
-              <button className="btn-2" onClick={() => navigate('/about')}>
-                About Us
-              </button>
+      <div className="main-container">
+        <div className="home-container">
+          <div className="top">
+            <div className="top-left">
+              <h1 className="top-left-1">SPECTASTYLE</h1>
+              <h6 className="top-left-2-h6">Your one stop Sneaker Shop!</h6>
+              <h6 className="top-left-2">
+                Welcome to our premium e-commerce shoe haven! Discover stylish and
+                comfortable footwear, showcasing the latest trends and timeless
+                classics. From casual kicks to elegant heels, find your perfect
+                pair and step into fashion-forward confidence with us.
+              </h6>
+              <div className="btn-container">
+                <button className="btn-1" >
+                  <a href="#products">Products</a>
+                </button>
+                <button className="btn-2" onClick={() => navigate('/about')}>
+                  About Us
+                </button>
+              </div>
             </div>
+            <Tilt
+              scale={1.05}
+              transitionSpeed={3000}
+              tiltMaxAngleX={20}
+              tiltMaxAngleY={20}
+              perspective={1000}>
+              <div className="top-right ">
+                <img src={Image} alt="Sneaker" />
+              </div>
+            </Tilt>
           </div>
-          <Tilt
-            scale={1.05}
-            transitionSpeed={3000}
-            tiltMaxAngleX={20}
-            tiltMaxAngleY={20}
-            perspective={1000}>
-            <div className="top-right ">
-              <img src={Image} alt="Sneaker" />
-            </div>
-          </Tilt>
+          <div className="down-arrow">
+            <img src={Arrow} alt="Down Arrow" id="arrow" />
+          </div>
         </div>
-        <div className="down-arrow">
-          <img src={Arrow} alt="Down Arrow" id="arrow" />
-        </div>
-      </div>
-      <HomeSecond />
-      <Product />
+        <HomeSecond />
+        <Product />
+      </div >
     </Fragment>
   );
 };
