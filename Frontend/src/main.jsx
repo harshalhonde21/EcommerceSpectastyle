ce491df30dfb987c05186597d84ae098b2ab5ca4
Chalsi Jain <100270122+Chelsea67jain@users.noreply.github.com>
2024-08-10 18:14:10 +0530

    Updated main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -7,11 +7,13 @@ import FeedbackButton from "./Components/Feedbtn";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+     <GoogleOAuthProvider clientId="758500021538-7tm7mv4tas3ouma9bb0uau1ia209al78.apps.googleusercontent.com">
     <CartProvider>
       <BrowserRouter>
         <App />
         <FeedbackButton />        
       </BrowserRouter>
     </CartProvider>
+       </GoogleOAuthProvider>
   </React.StrictMode>
 );
